a40dfa89dc2a3ba8049cee67bde48373a448529e
Tom Bauer <tbauer1898@gmail.com>
2021-03-12 14:54:36 -0600

    hooked up app to jinja templating and made basic layout with semantic ui

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
+
 @app.get("/")
-async def root():
-    return {"message" : "hello world"}
+def dashboard(request : Request):
+    """
+    displays stock screener dashboard
+    """
+    return templates.TemplateResponse("dashboard.html", {
+        "request" : request
+    } )
+
+
+# run server with uvicorn main:app --reload
+
+@app.post("/stock")
+def create_stock():
+    """
+    creates stock and stores it in database
+    """
+    return {
+        "status" : "success",
+        "message" : "stock created"
+    }
